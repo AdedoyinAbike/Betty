02dc4c41c88d34cc16f4d19e5090b0921545be72
AdedoyinAbike <habeebahadedoyinoyatayo@gmail.com>
2023-10-21 08:39:17 -0700

    c program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 /**
- *main - Entry point of the program
+ * main - Entry point of the program
  *
- *Decription: This is the main functon that print the siz of intrinsic data
+ * Decription: Print the size of various types
  *
- *Return - Always 0 (success) 
+ * Return - Always 0 (success) 
  */
 int main(void)
 {
-  printf("Size of a char: %d byte(s)\n", sizeof(char));
-  printf("Size of an int: %d byte(s)\n", sizeof(int));
-  printf("Size of a long int: %d byte(s)\n", sizeof(long));
-  printf("Size of a long long int: %d byte(s)\n", sizeof(long long));
-  printf("Size of a float: %d byte(s)\n", sizeof(float));
+  printf("Size of a char: %ld byte(s)\n", sizeof(char));
+  printf("Size of an int: %ld byte(s)\n", sizeof(int));
+  printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+  printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+  printf("Size of a float: %ld byte(s)\n", sizeof(float));
   return (0);
+}

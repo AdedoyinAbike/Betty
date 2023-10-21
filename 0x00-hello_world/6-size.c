6461f1d9c6a0e7c422f6434b56ce24d93d2762d5
AdedoyinAbike <habeebahadedoyinoyatayo@gmail.com>
2023-10-21 07:34:29 -0700

    c program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-  printf("Size of a char: %lu bytes\n", sizeof(char));
-  printf("Size of an int: %bytes\n", sizeof(int));
-  printf("Size of a long int: %lu bytes\n", sizeof(long));
-  printf("Size of a long long int: %lu bytes\n", sizeof(long long));
-  printf("Size of a float: %lu bytes\n" sizeof(float));
+  printf("Size of a char:\n", sizeof(char));
+  printf("Size of an int:\n", sizeof(int));
+  printf("Size of a long int:\n", sizeof(long));
+  printf("Size of a long long int:\n", sizeof(long long));
+  printf("Size of a float:\n" sizeof(float));
   return (0);
 }
  

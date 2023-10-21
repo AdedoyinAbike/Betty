81e932e1f4e42247be8f967faad6d9563c377c56
AdedoyinAbike <habeebahadedoyinoyatayo@gmail.com>
2023-10-21 09:37:17 -0700

    c program

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 
 /**
- * main - Entry point of a program
+ * main - Entry point
  *
- * Return - 1(indicates an error)
+ * Description: Print the message to standard error and return 1
+ *
+ * Return: Always 1
  */
 int main(void)
 {
     const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, message, strlen(message));
+    write(2, message, 59);
     return (1);
-
+}

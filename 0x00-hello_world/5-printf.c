44acda49ef5abf624be593c4d1000c7f48a909e6
AdedoyinAbike <habeebahadedoyinoyatayo@gmail.com>
2023-10-21 07:46:22 -0700

    c program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point of the program
  *
- * Return: Always 0 (success)
+ * Return: Always 0
  */
 int main(void)
 {
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-  printf("Size of a char:\n", sizeof(char));
-  printf("Size of an int:\n", sizeof(int));
-  printf("Size of a long int:\n", sizeof(long));
-  printf("Size of a long long int:\n", sizeof(long long));
-  printf("Size of a float:\n" sizeof(float));
+  printf("Size of a char: %ld bytes\n", sizeof(char));
+  printf("Size of an int: %ld bytes\n", sizeof(int));
+  printf("Size of a long int: %ld bytes\n", sizeof(long));
+  printf("Size of a long long int: %ld bytes\n", sizeof(long long));
+  printf("Size of a float: %ld bytes\n" sizeof(float));
   return (0);
 }
  
